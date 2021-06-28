b0b669c47cd5ca058a2bcea9f8e57979651ae5bf
dclemon <63597032+dclemon@users.noreply.github.com>
2021-06-28 21:33:09 +0800

    Add files via upload

---
+++ jd_config.py
@@ -0,0 +1,63 @@
+import os
+import configparser
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+def getmidstring(html, start_str, end):
+    start = html.find(start_str)
+    if start >= 0:
+        start += len(start_str)
+        end = html.find(end, start)
+        if end >= 0:
+            return html[start:end].strip()
+
+
+def write_ini(inikey, inivaluse, str, filepath):
+    config = configparser.ConfigParser()
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    config.read(parent_dir + "/" + filepath, encoding='utf-8')
+    convaluse = config.set(inikey, inivaluse, str)
+    config.write(open(parent_dir + "/" + filepath, "w"))
+    return convaluse
+
+
+def read_ini(inikey, inivaluse, filepath):
+    config = configparser.RawConfigParser()
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    config.read(parent_dir + "/" + filepath, encoding='utf-8')
+    convaluse = config.get(inikey, inivaluse)
+    return convaluse
+
+
+def jd_checklogin(cokstr):
+    manual_cookies = {}
+    for item in cokstr.split(';'):
+        name, value = item.strip().split('=', 1)
+        manual_cookies[name] = value
+    cookie = manual_cookies
+    url = "https://try.jd.com/activity/getActivityList"
+    res = requests.get(url)
+    html_str = res.text
+    besoup = BeautifulSoup(html_str, features='lxml')
+    div_str = str(besoup.find_all('div', attrs={'class': "con"}))
+    items = BeautifulSoup(div_str, "html.parser")
+    a = items.find_all('li')
+    b = getmidstring(str(a[1]), 'activity_id="', '"')
+    url = 'https://try.jd.com/migrate/getActivityById?id=' + b
+    head = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/75.0.3770.142 Safari/537.36',
+        'ContentType': 'text/html; charset=utf-8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
+        'Connection': 'keep-alive',
+        'Host': 'try.jd.com',
+        'Referer': 'https://try.jd.com/public'
+    }
+    res = requests.get(url, headers=head, cookies=cookie)
+    c = json.loads(res.text)
+    d = c['data']['login']
+    print("是否登录：" + str(d))
+    return d, cookie
